874bdab8b60d778a63c653df856ded6c1295a79c
Pedro <pedro.garbe07@gmail.com>
2026-07-06 20:47:15 -0300

    Ajustando o FlexBox

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ let desejo_usuario = prompt("O que deseja com o meu site?");
 
 document.getElementById("titulo").textContent = "Bem Vindo ao meu site pessoal, " + nome_usuario + "!";4
 
-document.getElementById("cumprimentar").textContent = "Olá, " + nome_usuario + ", eu sou o Pedro Garbe!";
+document.getElementById("cumprimentar").textContent = "Olá " + nome_usuario + ", eu sou o Pedro Garbe!";
 
 if (idade_usuario <= 22) {
     alert ("Salve " + nome_usuario + ", eu sou o Pedro Garbe Desenvolvedor Web, aproveite meu site!");
