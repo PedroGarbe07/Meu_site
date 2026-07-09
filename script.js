3eaabb65f2f7772f43ad24da6ffb6f18d44a4051
Pedro <pedro.garbe07@gmail.com>
2026-07-09 15:00:26 -0300

    Ajustando o JavaScript

--- script.js
+++ script.js
@@ -4,10 +4,13 @@ let nome_usuario = prompt("Qual é o seu nome?");
 let idade_usuario = prompt("Qual é a sua idade?");
 let desejo_usuario = prompt("O que deseja com o meu site?");
 
-document.getElementById("titulo").textContent = "Bem Vindo ao meu site pessoal, " + nome_usuario + "!";4
+document.getElementById("titulo").textContent = "Bem Vindo ao meu site pessoal, " + nome_usuario + "!";
 
-document.getElementById("cumprimentar").textContent = "Olá " + nome_usuario + ", eu sou o Pedro Garbe!";
-
+if (idade_usuario <= 22) {
+    document.getElementById("cumprimentar").textContent = "Salve " + nome_usuario + ", eu sou o Pedro Garbe!";
+}else {
+    document.getElementById("cumprimentar").textContent = "Olá " + nome_usuario + ", eu sou o Pedro Garbe!";
+}
 if (idade_usuario <= 22) {
     alert ("Salve " + nome_usuario + ", eu sou o Pedro Garbe Desenvolvedor Web, aproveite meu site!");
 } else {
